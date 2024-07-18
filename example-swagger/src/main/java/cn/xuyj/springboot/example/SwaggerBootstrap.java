28fe49542aa8db1831cd459e74400b61fe38e233
徐永杰 <xuyj@dist.com.cn>
2024-07-18 15:29:27 +0800

    feat:example-swagger

---
+++ example-swagger/src/main/java/cn/xuyj/springboot/example/SwaggerBootstrap.java
@@ -0,0 +1,15 @@
+package cn.xuyj.springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuyj
+ * @since 2024/7/18 15:07
+ */
+@SpringBootApplication
+public class SwaggerBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerBootstrap.class, args);
+    }
+}
---
+++ example-swagger/src/main/java/cn/xuyj/springboot/example/bean/User.java
@@ -0,0 +1,14 @@
+package cn.xuyj.springboot.example.bean;
+
+import lombok.Data;
+
+/**
+ * @author xuyj
+ * @since 2024/7/18 15:16
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+}
