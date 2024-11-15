6d08e9cbe683610a6b1b99ec0e69607ad8c246f0
徐永杰 <xuyj@dist.com.cn>
2024-11-15 15:18:16 +0800

    feat:程序启动输出swagger地址

--- example-swagger/src/main/java/cn/xuyj/springboot/example/SwaggerBootstrap.java
+++ example-swagger/src/main/java/cn/xuyj/springboot/example/SwaggerBootstrap.java
@@ -1,15 +1,38 @@
 package cn.xuyj.springboot.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author xuyj
  * @since 2024/7/18 15:07
  */
 @SpringBootApplication
+@Slf4j
 public class SwaggerBootstrap {
-    public static void main(String[] args) {
-        SpringApplication.run(SwaggerBootstrap.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext context = SpringApplication.run(SwaggerBootstrap.class, args);
+        Environment env = context.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t" +
+                        "Doc: \thttp://{}:{}{}/swagger-ui.html\n" +
+                        "\t---Staff Mailbox started successfully\n" +
+                        "\t---启动成功---\n" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getProperty("server.servlet.context-path"));
     }
 }
