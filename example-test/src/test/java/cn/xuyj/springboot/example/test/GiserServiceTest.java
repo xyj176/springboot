72782b3881f56bdfc48bcc1ee8865c69502534e8
徐永杰 <xuyj@dist.com.cn>
2024-11-21 14:45:05 +0800

    feat:去除多余的引用

--- example-test/src/test/java/cn/xuyj/springboot/example/test/GiserServiceTest.java
+++ example-test/src/test/java/cn/xuyj/springboot/example/test/GiserServiceTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
 
 /**
  * @author xuyj
