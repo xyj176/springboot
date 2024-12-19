a9223c80f9d92bb02c97b718020da22c58f8435a
徐永杰 <xuyj@dist.com.cn>
2024-12-19 14:25:42 +0800

    feat:获取异步线程返回值

--- example-async/src/main/java/cn/xuyj/springboot/example/async/controller/TestController.java
+++ example-async/src/main/java/cn/xuyj/springboot/example/async/controller/TestController.java
@@ -1,10 +1,14 @@
 package cn.xuyj.springboot.example.async.controller;
 
+import cn.xuyj.springboot.example.async.domain.User;
 import cn.xuyj.springboot.example.async.service.TestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * @author xuyj
@@ -36,5 +40,25 @@ public class TestController {
         log.info("主线程开始");
         service.asyncMethodWithThreadPool();
         log.info("主线程结束");
+    }
+
+    @GetMapping("/async/return")
+    public String testAsyncWithReturn() throws ExecutionException, InterruptedException {
+        log.info("主线程开始");
+        Future<String> future = service.asyncMethodWithReturn();
+        String result = future.get();//get方法会阻塞主线程：只有当异步方法返回结果才会继续往下执行
+        log.info("异步方法返回值：" + result);
+        log.info("主线程结束");
+        return result;
+    }
+
+    @GetMapping("/async/return/user")
+    public User testAsyncWithReturn2() throws ExecutionException, InterruptedException {
+        log.info("主线程开始");
+        Future<User> future = service.asyncMethodWithReturn2();
+        User result = future.get();//get方法会阻塞主线程：只有当异步方法返回结果才会继续往下执行
+        log.info("异步方法返回值：" + result);
+        log.info("主线程结束");
+        return result;
     }
 }
--- example-async/src/main/java/cn/xuyj/springboot/example/async/service/TestService.java
+++ example-async/src/main/java/cn/xuyj/springboot/example/async/service/TestService.java
@@ -1,9 +1,12 @@
 package cn.xuyj.springboot.example.async.service;
 
+import cn.xuyj.springboot.example.async.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -30,11 +33,34 @@ public class TestService {
 
     //指定线程池bean的名称，即可使用该线程池
     @Async("asyncThreadPoolTaskExecutor")
-    public void asyncMethodWithThreadPool(){
+    public void asyncMethodWithThreadPool() {
         log.info("异步线程开始");
         log.info("异步方法内部线程名称：" + Thread.currentThread().getName());
         sleep();
         log.info("异步线程结束");
+    }
+
+    //异步方法具有返回值的话，需要使用Future来接收回调值
+    @Async("asyncThreadPoolTaskExecutor")
+    public Future<String> asyncMethodWithReturn() {
+        log.info("异步线程开始");
+        log.info("异步方法内部线程名称：" + Thread.currentThread().getName());
+        sleep();
+        log.info("异步线程结束");
+        return new AsyncResult<>("hello async");
+    }
+
+    @Async("asyncThreadPoolTaskExecutor")
+    public Future<User> asyncMethodWithReturn2() {
+        log.info("异步线程开始");
+        log.info("异步方法内部线程名称：" + Thread.currentThread().getName());
+        sleep();
+        User user = new User();
+        user.setName("async");
+        user.setAge(18);
+        user.setSex("男");
+        log.info("异步线程结束");
+        return new AsyncResult<>(user);
     }
 
     //自定义私有方法
