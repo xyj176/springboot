1cdc6880c039b9cf4b4e4af4185236902fd9c46c
徐永杰 <xuyj@dist.com.cn>
2025-02-13 15:24:00 +0800

    feat：添加自定义异常示例类

---
+++ example-infrastructure/src/main/java/cn/xuyj/springboot/example/infrastructure/exception/CustomException.java
@@ -0,0 +1,11 @@
+package cn.xuyj.springboot.example.infrastructure.exception;
+
+/**
+ * @author xuyj
+ * @des 自定义异常类
+ * @since 2025/2/13 15:22
+ */
+public class CustomException extends RuntimeException{
+
+    private static final long serialVersionUID = -9215603581533139634L;
+}
